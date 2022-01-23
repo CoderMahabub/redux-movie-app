a48ad34fec5a1d1fc449619a521de13a9262a08c
CoderMahabub <codermahabub@gmail.com>
2022-01-23 23:35:38 +0600

    Load Movies

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import MovieListing from '../MovieListing/MovieListing';
+import movieApi from '../../common/apis/movieApi';
+import { APIKey } from '../../common/apis/MovieApiKey';
 
 const Home = () => {
-	return <div>Home</div>;
+	useEffect(() => {
+		const movieText = 'Harry';
+		const fetchMovies = async () => {
+			const response = await movieApi.get(`?apikey=${APIKey}&s=${movieText}&type=movie`).catch((err) => {
+				console.log('Err :', err);
+			});
+			console.log(response);
+		};
+		fetchMovies();
+	});
+
+	return (
+		<div className="banner-img">
+			<MovieListing />
+		</div>
+	);
 };
 
 export default Home;
